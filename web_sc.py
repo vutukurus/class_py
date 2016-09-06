95c09c83566b1968b55ecb11f91c01c640f5c121
safads <vutukurus@gmail.com>
2016-09-06 21:27:17 +0530

    beautiful soup 2 with overall

--- web_sc.py
+++ web_sc.py
@@ -9,11 +9,36 @@ url_path='http://www.bloomberg.com/quote/SPX:IND'
 return_data = urllib2.urlopen(url_path)
 
 #Return data use bs4 to faciliate our usage..
-obj_parse = BeautifulSoup(return_data,'html.parser')
-get_share_value=obj_parse.find('div',attrs={'class':'price'})
-print get_share_value
-print get_share_value.text
+html_doc = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
 
-get_index_value = obj_parse.find('h1',attrs={'class':'name'})
-print get_index_value
-print get_index_value.text
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+
+<p class="story">...</p>
+"""
+obj_parse = BeautifulSoup(html_doc,'html.parser')
+print obj_parse.title
+print obj_parse.find_all('p') #all hits..
+print obj_parse.find('p') #first hit..
+print "=========="
+print obj_parse.find(id="link2")
+
+process_href= obj_parse.find_all('a')
+for i in process_href:
+	print i.get('class')
+
+
+
+
+
+
+
+
+
+
