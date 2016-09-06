24d5369c1dfcdc3a55c4b16f8f26a143005f5a1d
safads <vutukurus@gmail.com>
2016-09-06 20:17:31 +0530

    changed version or fixed

--- new db program.py
+++ new db program.py
@@ -7,15 +7,15 @@ place1 = raw_input("enter path for place 1:")
 place2 = raw_input("enter path for place 2:")
 
 table_list_Places = sqlite3.connect("places.db")
-table_list_Places.execute("create table lists(place1 text,place2 text)")
+table_list_Places.execute("create table lists_temp_5(place1 text,place2 text)")
 
-place1_lists = os.listdir(place1)
-place2_lists = os.listdir(place2)
+place1_lists = os.listdir(r'C:\Users\siv\Desktop\new\class_py')
+place2_lists = os.listdir(r'C:\Users\siv\Desktop\automation')
+test="sreedhar"
+table_list_Places.execute("insert into lists_temp_5(place1,place2) values(1,'%s')" %(test))
+table_list_Places.commit()
 
-insert into lists(place1,place2) values(1,test)
-
-"""
 for i in place1_lists:
 	for j in place2_lists:
-		insert into lists(i,j) values (i,j)
-"""
+		table_list_Places.execute("insert into lists_temp_5(place1,place2) values('%s','%s')" %(i,j))
+table_list_Places.commit()
