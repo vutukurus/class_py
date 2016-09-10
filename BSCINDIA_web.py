074908edeb953f9b17c6d9b0f2cb788f0dfdc460
sreedha1986 <sreedhar.09.1986@gmail.com>
2016-09-10 14:48:24 +0530

    added new program

--- BSCINDIA_web.py
+++ BSCINDIA_web.py
@@ -9,6 +9,6 @@ url_path = "http://www.bseindia.com/"
 return_data = urllib2.urlopen(url_path)
 
 obj_parse = BeautifulSoup(return_data,'html.parser')
-get_share_value = obj_parse.find('div',attrs={'class':'newsensexvalue'})
+get_share_value = obj_parse.find('div id="tdsp"',attrs={'class':'newsensexvalue'})
 
 print "BSC SENSE Value:",get_share_value
