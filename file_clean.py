85e876ec41ca81bc395b393860c6063e5c2c538a
safads <vutukurus@gmail.com>
2017-09-18 20:03:22 +0530

    to show you

--- file_clean.py
+++ file_clean.py
@@ -2,6 +2,8 @@ file_name = r"C:\Users\Sreedhar\Desktop\log.txt"
 new_file =r"C:\Users\Sreedhar\Desktop\cleaned.txt"
 
 #This is a code change for os module demo
+#this is a line added to show how to checkin
+#checkin- from ur local pc to repository saving..
 
 c= open(new_file,'w')
 
