3d94aa2763dcebe9ec6d0f2860240c3256427a83
safads <vutukurus@gmail.com>
2017-10-17 19:14:27 +0530

    This is a demo change

--- dict_list_conv.py
+++ dict_list_conv.py
@@ -1,18 +1,3 @@
-a=set([1,2,3,4,5,6])
-b=[]
-for i in a:
-	print i
-	b.append(i)
-print b
-print "========="
-
-for i in a:
-	#print j
-	if j+1 > len(a):
-		break
-	
-	d[a[j]] = a[j+1]
-	j=j+2
 	
 	
 print d	
